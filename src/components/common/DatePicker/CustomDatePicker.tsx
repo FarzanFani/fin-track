2b63f71567ae50575dc0849206a3d692a9468e79
Farzan Fani <mrm776384@gmail.com>
2025-04-04 12:20:06 +0330

    feat: add date picker

--- src/components/common/DatePicker/CustomDatePicker.tsx
+++ src/components/common/DatePicker/CustomDatePicker.tsx
@@ -1,52 +1,60 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button, Pressable } from "react-native";
+import { Dispatch, useState } from "react";
+import { StyleSheet, Pressable } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import CustomInput from "../Input/Input";
-import BottomModal from "../Modal";
+import { format } from "date-fns";
+import { FieldError } from "react-hook-form";
 
-export default function CostumeDatePicker() {
-  const [date, setDate] = useState(new Date());
-  const [show, setShow] = useState(false);
+interface Props {
+  date?: Date;
+  setDate: Dispatch<React.SetStateAction<Date | undefined>>;
+  title?: string;
+  customFormat?: string;
+  customOnChange?: (selectedDate: Date) => void;
+  display?: "spinner" | "calendar";
+  mode: "date" | "time";
+  error?: FieldError;
+}
 
-  // Function to handle the date change
-  const onChange = (event: any, selectedDate: Date | undefined) => {
-    console.log(date);
+export default function CustomDatePicker(props: Props) {
+  const [showPicker, setShowPicker] = useState<boolean>(false);
+  const [text, setText] = useState("");
 
-    const currentDate = selectedDate || date; // if no date is selected, keep the previous date
-    setShow(false); // hide the picker
-    setDate(currentDate); // set the selected date
-  };
-
-  // Function to show the date picker
-  const showDatePicker = () => {
-    setShow(true); // show the picker when button is pressed
+  const onChange = (_, selectedDate?: Date) => {
+    if (selectedDate) {
+      setText(
+        format(selectedDate, props.customFormat ? props.customFormat : "MMMM dd, yyyy")
+      );
+      props.setDate(selectedDate);
+      props.customOnChange && props.customOnChange(selectedDate);
+    }
+    setShowPicker(false);
   };
 
   return (
     <>
-      <Pressable
-        style={{ width: "100%", backgroundColor: "white", height: 50 }}
-        onPress={() => setShow(true)}
-      >
-        {/* <CustomInput
-          value=""
-          onChangeText={() => setShow(true)}
-          placeholder="Add date"
+      <Pressable onPress={() => setShowPicker(true)} style={styles.container}>
+        <CustomInput
+          value={text}
+          setValue={() => {}}
+          readOnly
           title="Birth Date"
-          readOnly={true}
-        /> */}
+          placeholder="YYYY/MM/DD"
+          error={props.error}
+        />
       </Pressable>
-
-      {show && <DateTimePicker value={date} mode="date" onChange={onChange} />}
+      {showPicker && (
+        <DateTimePicker
+          value={props.date ?? new Date()}
+          mode={props.mode}
+          display="spinner"
+          onChange={onChange}
+        />
+      )}
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    marginBottom: 20,
-    backgroundColor: "white",
-    width: "100%",
-    textAlign: "center",
-  },
+  container: { width: "100%" },
 });
